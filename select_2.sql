939e721966ca7bcdb007c7e0a11c3703e6fb3e45
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-01-15 16:20:07 +0500

    vovel coutry solved

--- select_2.sql
+++ select_2.sql
@@ -57,3 +57,14 @@ WHERE LENGTH(name)=LENGTH(capital)
 SELECT name, capital
 FROM world
 WHERE LEFT(name,1) = LEFT(capital,1) AND name <> capital
+
+
+-- Find the country that has all the vowels and no spaces in its name.
+SELECT name
+FROM world
+WHERE name LIKE '%a%' 
+AND name LIKE '%e%' 
+AND name LIKE '%o%' 
+AND name LIKE '%i%' 
+AND name LIKE '%u%' 
+AND name NOT LIKE '% %' 
