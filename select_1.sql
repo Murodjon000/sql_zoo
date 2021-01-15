ebaf650133653c454ec3c2dbcd0943aabfa875f8
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-01-15 15:23:27 +0500

    germany population solved

--- select_1.sql
+++ select_1.sql
@@ -1,3 +1,9 @@
+-- Modify it to show the population of Germany
 SELECT name, population 
 FROM world
 WHERE name = 'Germany'
+
+-- Show the name and the population for 'Sweden', 'Norway' and 'Denmark'
+SELECT name, population 
+FROM world
+WHERE name IN ('Sweden', 'Norway', 'Denmark');
