9e2e8f0eb27aeddf84f57d40c4d0c795a3b44bb0
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-01-15 20:24:57 +0500

    europe gdp problem

--- select_4.sql
+++ select_4.sql
@@ -5,4 +5,8 @@ SELECT name FROM world
      (SELECT population FROM world
       WHERE name='Russia')
 
+-- Show the countries in Europe with a per capita GDP greater than 'United Kingdom'.
+SELECT name
+FROM world
+WHERE continent='Europe' AND gdp/population > (SELECT gdp/population from world WHERE name='United Kingdom')
 
