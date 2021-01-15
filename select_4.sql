75565b362e3ef46885fa03e1304318c54f5f08f3
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-01-15 20:26:00 +0500

    canada poland problem

--- select_4.sql
+++ select_4.sql
@@ -17,5 +17,9 @@ FROM world
 WHERE continent IN(SELECT continent FROM world WHERE name IN ('Argentina' ,'Australia'))
 ORDER BY name
 
+-- Which country has a population that is more than Canada but less than Poland? Show the name and the population.
+SELECT name,population
+FROM world
+WHERE population > (SELECT population FROM world WHERE name='Canada') AND population < (SELECT population FROM world WHERE name='Poland')
 
 
