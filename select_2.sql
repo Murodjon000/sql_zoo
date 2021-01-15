be2195277aa09ecf922d1cdc78d36c4132d29c8a
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-01-15 16:04:44 +0500

    Round nearest 1000

--- select_2.sql
+++ select_2.sql
@@ -41,3 +41,9 @@ WHERE area>3000000 XOR population>250000000
 SELECT name,ROUND(population/1000000,2),ROUND(gdp/1000000000,2)
 FROM world
 WHERE continent='South America'
+
+-- Show per-capita GDP for the trillion dollar countries to the nearest $1000.
+SELECT name, ROUND((gdp / population)/1000)*1000
+FROM world
+WHERE gdp>=1000000000000
+
