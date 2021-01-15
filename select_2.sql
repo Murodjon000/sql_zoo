22cf6230145ce943989c6a22f14a1f67f494e925
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-01-15 15:29:21 +0500

    solved at least population problem

--- select_2.sql
+++ select_2.sql
@@ -1,3 +1,8 @@
 --  Observe the result of running this SQL command to show the name, continent and population of all countries.
 SELECT name, continent, population 
 FROM world
+
+-- Show the name for the countries that have a population of at least 200 million. 
+SELECT name
+FROM world
+WHERE population >= 200000000
