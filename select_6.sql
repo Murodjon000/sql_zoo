7990dc790a2fe32face33b198bdb54f3af902151
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-01-16 23:14:03 +0500

    solved 2 problem in join method

--- select_6.sql
+++ select_6.sql
@@ -2,3 +2,8 @@
 SELECT matchid,player FROM goal 
   WHERE teamid LIKE 'GER%'
 
+-- Show id, stadium, team1, team2 for just game 1012
+SELECT DISTINCT id,stadium,team1,team2
+  FROM game
+JOIN goal ON game.id=1012 AND goal.matchid=1012
+
