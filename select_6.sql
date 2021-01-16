e1121ce88fcd67e626d7fe86ed729edbe6239b06
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-01-16 23:14:30 +0500

    solved 3 problem in join method

--- select_6.sql
+++ select_6.sql
@@ -7,3 +7,7 @@ SELECT DISTINCT id,stadium,team1,team2
   FROM game
 JOIN goal ON game.id=1012 AND goal.matchid=1012
 
+-- Modify it to show the player, teamid, stadium and mdate for every German goal.
+SELECT player,teamid,stadium,mdate
+  FROM game JOIN goal ON (id=matchid)
+WHERE teamid='GER'
