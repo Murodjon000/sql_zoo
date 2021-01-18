29ede446bf0b128323cd9f8b703fc793201eb601
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-01-18 14:41:38 +0500

    problem 7

--- slef_join.sql
+++ slef_join.sql
@@ -36,3 +36,11 @@ a.company = b.company AND a.num = b.num
 JOIN stops AS stopa ON a.stop=stopa.id
 JOIN stops AS stopb ON b.stop=stopb.id
 WHERE stopa.name = 'Fairmilehead' AND stopb.name = 'Tollcross';
+
+-- Give a list of all the services 
+-- which connect stops 115 and 137 ('Haymarket' and 'Leith')
+
+SELECT DISTINCT a.company, a.num
+FROM route a JOIN route b ON
+a.company = b.company AND a.num = b.num
+WHERE a.stop = 115 AND b.stop = 137;
