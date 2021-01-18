3ada7498bfb1adb8a2e390a1489133c16852e414
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-01-18 14:39:51 +0500

    problem 3

--- slef_join.sql
+++ slef_join.sql
@@ -6,3 +6,8 @@ FROM stops
 SELECT id
 FROM stops 
 WHERE name = 'Craiglockhart'
+
+-- Give the id and the name for the stops on the '4' 'LRT' service.
+SELECT id,name 
+FROM stops JOIN route ON (stop=id)
+WHERE num='4'  AND company='LRT'
