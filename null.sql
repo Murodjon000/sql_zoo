0e7cdc5dc21dc90565ab8ccb118d4ce60283c420
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-01-18 14:35:26 +0500

    null 3 problem

--- null.sql
+++ null.sql
@@ -7,3 +7,8 @@ WHERE dept IS NULL
 SELECT teacher.name, dept.name
  FROM teacher INNER JOIN dept
            ON (teacher.dept=dept.id)
+
+-- Use a different JOIN so that all teachers are listed
+SELECT teacher.name, dept.name
+ FROM teacher LEFT OUTER JOIN dept
+           ON (teacher.dept=dept.id)
